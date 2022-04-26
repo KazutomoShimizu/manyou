1b318fb8c45e431beebb93d2d0fc7d804238a651
kazutomo shimizu <kazknman@gmail.com>
2022-04-26 11:13:12 +0900

    blogコントローラー、モデルの作成

---
+++ db/migrate/20220425053748_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null:false
+      t.text :content, null:false
+
+      t.timestamps
+    end
+  end
+end
