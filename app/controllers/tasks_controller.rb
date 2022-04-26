4fffd6c2595a4ade85e0984785bbcbe68dcd1831
kazutomo shimizu <kazknman@gmail.com>
2022-04-26 11:46:58 +0900

    新規作成、詳細画面のコントローラー編集、ページの作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,29 @@
 class TasksController < ApplicationController
 
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path, notice: "タスクを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
 end
