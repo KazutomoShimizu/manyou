f0f4cd1e37e384cf8ba0686c2c0a908edb457034
kazutomo shimizu <kazknman@gmail.com>
2022-04-26 14:14:35 +0900

    削除機能、編集機能、確認画面のview,controllerの設定及び作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -10,20 +11,48 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to task_path, notice: "タスクを作成しました"
-    else
+    if params[:back]
       render :new
+    else
+      if @task.save
+        redirect_to task_path, notice: "タスクを作成しました"
+      else
+        render :new
+      end
     end
   end
 
   def show
-    @task = Task.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました"
+  end
+
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
 end
