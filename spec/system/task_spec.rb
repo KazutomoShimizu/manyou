b0f27a2841131cff44f69aee1d0d5cc285e0ffbe
kazutomo shimizu <kazknman@gmail.com>
2022-05-07 00:30:11 +0900

    systemrspecの修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
+    context 'タスクを新規作成した場合ステータスも登録できる' do
       it '作成したタスクが表示される' do
-        task = FactoryBot.create(:task, title: 'sample1',content: 'sample1')
+        task = FactoryBot.create(:task, title: 'sample1',content: 'sample1',status: '未着手')
         visit new_task_path
         fill_in 'task_title', with:'sample1'
         fill_in 'task_content', with:'sample1'
+        select '未着手', from: 'task_status'
         click_on '登録する'
         expect(page).to have_content 'sample1'
       end
@@ -23,9 +24,11 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
+        FactoryBot.create(:task)
+        FactoryBot.create(:second_task)
         visit tasks_path
-        task_title = all('.task_row')
-        expect(task_title[0]).to have_content 'sampletest'
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'sample2'
       end
     end
   end
@@ -37,17 +40,17 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit tasks_path
       end
       context '終了期限でソートするボタンを押した場合' do
-        it '終了期限が遅いタスクが一番上に表示される', :retry => 3, :retry_wait => 5  do
-         click_on "終了期限でソートする"
+        it '終了期限が遅いタスクが一番上に表示される' do
+         click_link "終了期限でソートする"
          task_title = all('.task_row')
-         expect(task_list[0]).to have_content "sample1"
+         expect(task_title[0]).to have_content "sample2"
         end
       end
       context '優先順位でソートするボタンを押した場合' do
         it '優先順位が高いタスクが一番上に表示される' do
-         click_on "優先度でソートする"
+         click_link "優先度でソートする"
          task_title = all('.task_row')
-         expect(task_list[0]).to have_content "sample2"
+         expect(task_title[0]).to have_content "sample1"
         end
       end
     end
@@ -61,9 +64,9 @@ RSpec.describe 'タスク管理機能', type: :system do
       context 'タイトルであいまい検索をした場合' do
         it "検索キーワードを含むタスクで絞り込まれる" do
           visit tasks_path
-          fill_in 'task_title', with: 'sample1'
+          fill_in 'inputcity', with: 'sample'
           click_on "検索"
-          expect(page).to have_content 'sample1'
+          expect(page).to have_content 'sample2'
         end
       end
       context 'ステータス検索をした場合' do
@@ -71,16 +74,16 @@ RSpec.describe 'タスク管理機能', type: :system do
           visit tasks_path
           select '着手中', from: 'task[status]'
           click_on "検索"
-          expect(page).to have_content 'sample1'
+          expect(page).to have_content 'sample2'
         end
       end
       context 'タイトルのあいまい検索とステータス検索をした場合' do
         it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
           visit tasks_path
-          fill_in 'task_title', with: 'sample1'
+          fill_in 'inputcity', with: 'sample'
           select '着手中', from: 'task[status]'
           click_on "検索"
-          expect(page).to have_content 'sample1'
+          expect(page).to have_content 'sample2'
         end
       end
     end
